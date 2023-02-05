6fdfc459ddb816bc3249fc668627896d83028bdf
matijaveselinovic94@yahoo.com <matijaveselinovic94@yahoo.com>
2023-02-05 23:57:18 +0100

    Add env file, update gitignore, update index.html

--- src/Components/SingleArticle/SingleArticle.tsx
+++ src/Components/SingleArticle/SingleArticle.tsx
@@ -1,6 +1,5 @@
 import './SingleArticle.css';
 import { Article } from '../../Misc/types';
-import { API_IMAGE_ENDPOINT, API_ARTICLE_ENDPOINT } from '../../api/ArticlesApi';
 
 type SingleArticleProps = {
     articleData: Article,
@@ -12,13 +11,13 @@ export function SingleArticle({articleData, deleteArticle}: SingleArticleProps) 
     return (
     <div className='singleArticleContainer'>
         <div className={'singleArticleDelete'} onClick={() => deleteArticle(articleData.slug)}>X</div>
-        <div className='singleArticleImageContainer'><a href={API_ARTICLE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer"><img className='singleArticleImage' alt={articleData.title} src={API_IMAGE_ENDPOINT + articleData.post_image}></img></a></div>
+        <div className='singleArticleImageContainer'><a href={process.env.REACT_APP_API_IMAGE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer"><img className='singleArticleImage' alt={articleData.title} src={process.env.REACT_APP_API_IMAGE_ENDPOINT + articleData.post_image}></img></a></div>
         <div className='singleArticleTextContainer'>
-        <p className='singleArticleTitle'><a className='unstyledLink' href={API_ARTICLE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer">{articleData.title}</a></p>
+        <p className='singleArticleTitle'><a className='unstyledLink' href={process.env.REACT_APP_API_IMAGE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer">{articleData.title}</a></p>
             <p className='singleArticleDate'>{new Date(articleData.date).toLocaleDateString()}</p>
             <p className='singleArticleExcerpt'>{articleData.excerpt.substring(3, articleData.excerpt.length - 6).replaceAll('&#39;', "'")}</p>
         </div>
-        <a className='singleAriticleLink' href={API_ARTICLE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer" >Full Article</a>
+        <a className='singleAriticleLink' href={process.env.REACT_APP_API_ARTICLE_ENDPOINT + articleData.slug} target='_blank' rel="noreferrer" >Full Article</a>
     </div>
     )
 }
--- src/api/ArticlesApi.tsx
+++ src/api/ArticlesApi.tsx
@@ -1,13 +1,9 @@
-import axios from "axios"
-import { useQuery } from "react-query"
-import { Article } from "../Misc/types"
-
-export const API_ENDPOINT = 'https://www.alpha-orbital.com/last-100-news.json'
-export const API_IMAGE_ENDPOINT = 'https://www.alpha-orbital.com/assets/images/post_img/'
-export const API_ARTICLE_ENDPOINT = 'https://www.alpha-orbital.com/news/'
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Article } from "../Misc/types";
 
 export function fetchArticles(): Promise<Article[]> {
-    return axios.get(API_ENDPOINT).then((response) => response.data)
+    return axios.get(process.env.REACT_APP_API_ENDPOINT as string).then((response) => response.data)
 }
 
 export function useArticles() {
